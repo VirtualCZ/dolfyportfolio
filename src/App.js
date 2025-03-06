0717062ae4f009681ed2670f3fa1244c37d3fe33
Tomáš Gabriel <tomas.gabriel@sitewell.cz>
2025-03-06 21:52:51 +0100

    initial commit with the main layout done :3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
+import { Col, Container, Row, Stack } from "react-bootstrap";
+import "./built-styles/main.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="hero-section">
+          <nav>
+            <span className="nav-text">dolfy</span>
+          </nav>
+          <img src="/temp-photos/hero_bg.jpg" className="hero-bg"></img>
+          <Container className="hero">
+            <Row>
+              <Col className="hero-left-col d-lg-block" lg={5} xl={5}>
+                <img className="hero-top-img" src="/temp-photos/IMG_1695.JPG" />
+              </Col>
+              <Col
+                className="hero-center-col d-none d-lg-none d-xxl-block"
+                xl={1}
+              />
+              <Col className="hero-right-col" lg={7} xxl={6}>
+                <div className="hero-card">
+                  <h2>
+                    hey there, I'm <span className="pink-color">dolfy</span>
+                  </h2>
+                  <p>
+                    "Not Your Typical Furry"
+                    <br />
+                    Fursuiter & Photographer
+                  </p>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <Container className="about-me">
+          <h1>About Me</h1>
+          <Row>
+            <Col className="about-me-left" lg={12} xl={7}>
+              <Stack gap={3}>
+                <div>
+                  <h2>About Me</h2>
+                  <p>
+                    24yr, Bipolar, Slightly Autistic I like History of Wehrmacht
+                    & Luftwaffe(NO i don’t support the NSDAP - Nazi Party) So
+                    you might see me running around in an Uniform from time to
+                    time.
+                    <br />
+                    I'm a Photographer so you can ask me for photos. (Based in
+                    Ostrava, Czechia)
+                    <br />
+                    <br />
+                    Don’t take me seriously, life is short so don’t take your
+                    time to complain, but rather adapt to behaviors of others.
+                  </p>
+                </div>
+                <div>
+                  <h2>Social Media</h2>
+                  <div>
+                    <a
+                      href="https://www.instagram.com/dolfythefolf/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Instagram
+                    </a>
+                    <a
+                      href="https://t.me/DOLFYWASTAKEN"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Telegram
+                    </a>
+                  </div>
+                </div>
+                <div>
+                  <h2>Get In Touch</h2>
+                  <div>
+                    <a
+                      href="mailto:info@dolfythe.dev"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Email
+                    </a>
+                    <a
+                      href="https://t.me/DOLFYWASTAKEN"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Telegram
+                    </a>
+                  </div>
+                </div>
+              </Stack>
+            </Col>
+            <Col className="gallery-preview" lg={12} xl={5}>
+              <div className="gallery-preview-bg"></div>
+              <Row className="gallery-button-container">
+                <Col lg={12}>
+                  <button>open gallery</button>
+                </Col>
+              </Row>
+              <Stack gap={3}>
+                <Stack direction="horizontal" gap={3}>
+                  <img src="/temp-photos/IMG_0153.jpg" />
+                  <img src="/temp-photos/IMG_0153.jpg" />
+                  <img src="/temp-photos/IMG_0153.jpg" />
+                </Stack>
+                <Stack direction="horizontal" gap={3}>
+                  <img src="/temp-photos/IMG_0153.jpg" />
+                  <img src="/temp-photos/IMG_0153.jpg" />
+                  <img src="/temp-photos/IMG_0153.jpg" />
+                </Stack>
+                <Stack direction="horizontal" gap={3}>
+                  <img src="/temp-photos/IMG_0153.jpg" />
+                  <img src="/temp-photos/IMG_0153.jpg" />
+                  <img src="/temp-photos/IMG_0153.jpg" />
+                </Stack>
+              </Stack>
+            </Col>
+          </Row>
+        </Container>
       </header>
     </div>
   );
